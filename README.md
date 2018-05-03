068a1020aa2dbea076b63cda1ee62d57be2f39c0
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-05-03 17:38:45 -0500

    Etiquetas

--- README.md
+++
@@ -1,2 +0,0 @@
-# Gráficas de espectros de absorción
-Grafica los espectros de absorción de los datos obtenidos en formato ASCII.
--- espectros.py
+++ espectros.py
@@ -2,6 +2,7 @@
 formato ASC'''
 
 import matplotlib.pyplot as plt
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 import Archivos
@@ -38,14 +39,24 @@ def getDatos(archivo,indice):
 
 '''Grafica los datos y asigna el nombre a la grafica de acuardo al nombre del
 archivo'''
-def grafica(datos,nombre):
+def grafica(datos,nombre,bandas):
     datosx=[]
     datosy=[]
+    minimos=[]
     for i in range(len(datos)):
         datosx.append(datos[i][0])
         datosy.append(datos[i][1])
-    plt.figure(figsize=(30,10))
+        if(1<i<len(datos)-1 and datos[i-1][1]>datos[i][1] and
+           datos[i][1]<datos[i+1][1]) and datos[i][1]<95 :
+                minimos.append([datos[i][0],datos[i][1]])
+    fig=plt.figure(figsize=(20,10))
     plt.plot(datosx,datosy)
+    ax = fig.add_subplot(111)
+    for i in bandas.keys():
+        for j in minimos:
+            if (j[0]-15<bandas.get(i)<j[0]+15):
+                ax.annotate(i, xy=(j[0],j[1]), xytext=(j[0],j[1]),
+                arrowprops=dict(facecolor='black'))
     plt.title(nombre)
     plt.xlabel('Número de onda [cm⁻¹]')
     ax = plt.gca()
@@ -62,35 +73,59 @@ def getNombresArchivos(directorio,primerArchivo):
 
 '''Grafica individualmente los datos de todos los archivos dentro de un
 directorio'''
-def graficaTodos(directorio,listaDeArchivos):
+def graficaTodos(directorio,listaDeArchivos,bandas):
     for i in listaDeArchivos:
         f=open(directorio+i)
         archivo=f.read()
         datos=getDatos(archivo,limpia(archivo))
-        grafica(datos,i[:len(i)-4])
+        grafica(datos,i[:len(i)-4],bandas)
 
 '''Grafica sobreponiendo los datos de todos los archivos dentro de un
 directorio'''
 def graficaSobrepuestas(directorio,listaDeArchivos):
-    plt.figure(figsize=(30,10))
+    fig = plt.figure(figsize=(20,10))
     plt.title('Sobrepuestas')
     plt.xlabel('Número de onda [cm⁻¹]')
     plt.ylabel('Transmitancia [%]')
-    for i in listaDeArchivos:
-        f=open(directorio+i)
+    minimos=[]
+    for dir in listaDeArchivos:
+        f=open(directorio+dir)
         archivo=f.read()
         datos=getDatos(archivo,limpia(archivo))
         datosx=[]
         datosy=[]
-        for j in range(len(datos)):
-            datosx.append(datos[j][0])
-            datosy.append(datos[j][1])
-        plt.plot(datosx,datosy,label=i[:len(i)-4])
+        minimos=[]
+        for i in range(len(datos)):
+            datosx.append(datos[i][0])
+            datosy.append(datos[i][1])
+            if(1<i<len(datos)-1 and datos[i-1][1]>datos[i][1] and
+               datos[i][1]<datos[i+1][1]) and datos[i][1]<95 :
+                    minimos.append([datos[i][0],datos[i][1]])
+        plt.plot(datosx,datosy,label=dir[:len(dir)-4])
         plt.legend(loc=2)
+        ax = fig.add_subplot(111)
+    for i in bandas.keys():
+        for j in minimos:
+            if (j[0]-15<bandas.get(i)<j[0]+15):
+                ax.annotate(i, xy=(j[0],j[1]), xytext=(j[0],j[1]),
+                arrowprops=dict(facecolor='black'))
     ax = plt.gca()
     ax.set_xlim(ax.get_xlim()[::-1])
     plt.savefig('Sobrepuestas.png',dpi=400)
+    for i in bandas.keys():
+        for j in minimos:
+            if (j[0]-15<bandas.get(i)<j[0]+15):
+                axzoom=fig.add_subplot(111)
+                axzoom.set(xlim=(j[0]+400, j[0]-400),
+                           ylim=(j[1]-20, j[1]+20), autoscale_on=False,
+                           title='Zoom'+str(i))
+                plt.savefig( 'Zoom'+str(i)+'.png', dpi=400)
+
+'''Diccionario con las bandas de absorción de interés'''
+bandas={'Estiramiento simétrico CH3':2608,'Estiramiento CH3':3089.6,
+        'Estiramiento C=O':1619.81,'Estiramiento asimétrico NH2':2295.55,
+        'Estiramiento OH':3439.71}
 
 '''Uso de las funciones para obtener las gráficas'''
-graficaTodos('Archivos/',getNombresArchivos('Archivos','allende1.asc'))
+graficaTodos('Archivos/',getNombresArchivos('Archivos','allende1.asc'),bandas)
 graficaSobrepuestas('Archivos/',getNombresArchivos('Archivos','allende1.asc'))
